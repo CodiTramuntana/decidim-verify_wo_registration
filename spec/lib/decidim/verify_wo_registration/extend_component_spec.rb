be6fb5c319a87239d5e7e3627d7aa1aaee4a5773
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2023-06-15 19:16:55 +0200

    Make tests succeed

--- spec/lib/decidim/verify_wo_registration/extend_component_spec.rb
+++ spec/lib/decidim/verify_wo_registration/extend_component_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.shared_examples 'has global setting' do |attr_name, attr_type|
   it 'has attribute' do
-    attribute = global_settings.schema.attribute_set.find { |a| a.name == attr_name }
+    attribute = global_settings.attributes[attr_name]
     expect(attribute).to be_present
     expect(attribute.type).to eq(attr_type)
   end
@@ -15,13 +15,13 @@ describe 'extend gloal settings' do
     let!(:component_manifest) { Decidim.find_component_manifest :budgets }
     let(:global_settings) { component_manifest.settings(:global) }
 
-    it_behaves_like 'has global setting', :supports_without_registration, Axiom::Types::Boolean
+    it_behaves_like 'has global setting', :supports_without_registration, :boolean
   end
 
   context 'proposals settings' do
     let!(:component_manifest) { Decidim.find_component_manifest :proposals }
     let(:global_settings) { component_manifest.settings(:global) }
 
-    it_behaves_like 'has global setting', :supports_without_registration, Axiom::Types::Boolean
+    it_behaves_like 'has global setting', :supports_without_registration, :boolean
   end
 end
