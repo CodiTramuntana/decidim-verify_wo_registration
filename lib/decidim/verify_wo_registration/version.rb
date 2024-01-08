fb06541da2dae7fa4a07a4c71c86b4b88a419dde
Laura Jaime <laura.jv@coditramuntana.com>
2024-01-08 15:19:27 +0100

    Update Decidim version

--- lib/decidim/verify_wo_registration/version.rb
+++ lib/decidim/verify_wo_registration/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-meetings version.
   module VerifyWoRegistration
     VERSION = '0.3.0'
-    DECIDIM_VER = '>= 0.27'
+    DECIDIM_VER = '>= 0.28'
 
     def self.version
       VERSION
